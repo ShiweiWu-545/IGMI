cde16106149dfdbdc4ec0a802359b09c7481a167
ShiweiWu-545 <wushiwei@hrbeu.edu.cn>
2025-04-26 17:31:26 +0800

    commit

--- run/EvoEF2_run.py
+++
@@ -1,9 +0,0 @@
-from utils.data_process import *
-from pathlib import Path
-
-
-wt_path = Path('../data/1PPF_EI.pdb')
-path_mutation = Path('../data/individual_list.txt')
-out_path = Path('../data')
-
-make_input(wt_path, path_mutation, out_path)
